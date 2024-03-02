314642ef0b7780e921877726aca1ba4f80ec999c
Cecilia Sanare <ceci@sanare.dev>
2024-03-01 19:48:26 -0600

    fix: correct gnome cursor

--- mixins/nixos/desktop/gnome/default.nix
+++ mixins/nixos/desktop/gnome/default.nix
@@ -37,6 +37,13 @@ with lib.gvariant;
       dotfiles.desktop = {
         enable = mkDefault true;
         background = mkDefault "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src}";
+
+        cursor = mkDefault {
+          enable = true;
+          url = "https://github.com/ful1e5/apple_cursor/releases/download/v2.0.0/macOS-BigSur.tar.gz";
+          hash = "sha256-VZWFf1AHum2xDJPMZrBmcyVrrmYGKwCdXOPATw7myOA=";
+          name = "macOS-BigSur";
+        };
       };
 
       gtk = {
