dae6a6f2fe1531cb321039519e17e8b0b8bb1e11
Cecilia Sanare <ceci@sanare.dev>
2024-03-01 19:07:52 -0600

    fix: tweaks to nushell

--- mixins/nixos/shells/nushell/default.nix
+++ mixins/nixos/shells/nushell/default.nix
@@ -24,12 +24,10 @@
         enable = true;
         settings = {
           add_newline = true;
-          character = {
-            success_symbol = "[➜](bold green)";
-            error_symbol = "[➜](bold red)";
-          };
         };
       };
+
+      carapace.enable = true;
     };
   }];
 }
