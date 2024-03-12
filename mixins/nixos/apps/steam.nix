626adb717ca599dc9fbe16525a066fe1158d09b7
Cecilia Sanare <ceci@sanare.dev>
2024-03-12 11:29:37 -0500

    feat: use vkbasalt for x11 and gamescope for wayland

--- mixins/nixos/apps/steam.nix
+++ mixins/nixos/apps/steam.nix
@@ -1,9 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
 
+let 
+  isWayland = config.services.xserver.displayManager.gdm.wayland;
+  isNotWayland = !isWayland;
+in 
 {
   programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = isWayland;
 
   environment.systemPackages = with pkgs; [
     protonup-qt
-  ];
+  ] ++ lib.optional isNotWayland vkbasalt-cli;
 }
