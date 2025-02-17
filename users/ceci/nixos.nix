68783cb0e544efc2d479feb2d4a6aa7116268876
Cecilia Sanare <ceci@sanare.dev>
2025-02-16 20:31:19 -0600

    chore: cleanup unused apps

--- users/ceci/nixos.nix
+++ users/ceci/nixos.nix
@@ -1,9 +1,13 @@
 # Default Shared Configuration
-{ username, pkgs, platform, ... }:
+{ username, pkgs, platform, lib, ... }:
 
 {
   imports = [
     ./shared.nix
+  ];
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "dotnet-sdk-6.0.428"
   ];
 
   users.users.${username}.hashedPassword = "$6$P9EJMHnu8b/OVVRS$1qECnQmYav5EhbnTOk3mnO3dBlMOAsF9/lgCRONwO/GHUfHZFIpxYSyOTPqpv6S6dqO4uSxSZ9KMDl5yX9AHH1";
@@ -13,6 +17,7 @@
     users = [ username ];
   };
 
+  services.fwupd.enable = true;
   services.mullvad-vpn = {
     enable = true;
     enableExcludeWrapper = true;
