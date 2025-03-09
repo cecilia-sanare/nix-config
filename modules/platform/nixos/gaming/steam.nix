f1e9ce98559ec49a2df90b2fdb5b49c3f3fcb209
Cecilia Sanare <ceci@sanare.dev>
2025-03-09 18:37:37 -0500

    fix(steam): resolve issue with in-game clocks being wrong

--- modules/platform/nixos/gaming/steam.nix
+++ modules/platform/nixos/gaming/steam.nix
@@ -15,6 +15,12 @@ in
   config = mkIf (cfg.enable && cfg.steam.enable) {
     programs.steam.enable = true;
     programs.steam.gamescopeSession.enable = true;
+    # Fix in-game clocks from being wrong.
+    programs.steam.package = pkgs.steam.override {
+      extraProfile = ''
+        unset TZ
+      '';
+    };
 
     environment.systemPackages = with pkgs; [
       protonup-qt
